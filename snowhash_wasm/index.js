3d29de463768099f603b0193efb313602751cd7c
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-05-20 22:07:03 +1000

    Update html and js serving snowhash_wasm

--- snowhash_wasm/index.js
+++ snowhash_wasm/index.js
@@ -1,55 +1,76 @@
-const snowhash_wasm = import('./snowhash_wasm');
+let snowhash_wasm = import('./snowhash_wasm');
 
 snowhash_wasm.then(snowhash => {
-    var canvas = $('#hexgrid');
-  
-    const opts = {
-      scale: Number(canvas.attr('scale')),
-      canvasSize: Number(canvas.attr('size')),
-    };
+    let hashInput = $('#hashInput');
+    let canvas = $('#hexgrid');
 
-    var ctx = canvasContext(opts, canvas);
-    var hashInput = $('#hashInput');
+    let size = Number(canvas.attr('size'));
+    let scale = Number(canvas.attr('scale'));
 
-    hashInput.change(function() {
-        clearCanvas(ctx);
-        var points = JSON.parse(snowhash.generate(hashInput.val()));
-        drawPoints(ctx, opts, points);
+    setCanvasSize(canvas, size);
+    let ctx = getCanvasContext(canvas);
+
+    const opts = {ctx, size, scale, hashInput};
+
+    // Draw initial snowflake from hash..
+    const hash = randomHash();
+    update(opts, snowhash, hash);
+
+    // Input on change.
+    hashInput.on('input', function() {
+        update(opts, snowhash, hashInput.val());
     });
 
-    hashInput.val('aal3r389h12kjlh121kjh2g41k2h312l');
-    var points = JSON.parse(snowhash.generate(hashInput.val()));
-    drawPoints(ctx, opts, points);
+    // Canavs on click.
+    canvas.click(function() {
+        const hash = randomHash();
+        update(opts, snowhash, hash);
+    });
 });
 
-function canvasContext(opts, canvas) {
-    canvas.attr('width', opts.canvasSize);
-    canvas.attr('height', opts.canvasSize);
+function update(opts, snowhash, hash) {
+    const points = JSON.parse(snowhash.generate(hash));
 
-    var ctx = canvas[0].getContext('2d');
+    drawPoints(opts, points);
+    opts.hashInput.val(hash);
+}
+
+function randomHash() {
+    return sha256.update(Math.random().toString(36));
+}
+
+function setCanvasSize(canvas, size) {
+    canvas.attr('width', size);
+    canvas.attr('height', size);
+}
+
+function getCanvasContext(canvas) {
+    let ctx = canvas[0].getContext('2d');
+
     ctx.fillStyle = '#000000';
     ctx.lineWidth = 1;
 
     return ctx;
 }
 
-function clearCanvas(ctx) {
+function drawPoints(opts, points) {
+    let ctx = opts.ctx;
+
+    // Clear the canvas.
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-}
 
-function drawPoints(ctx, opts, points) {
-    for (var i = 0; i < points.length; i++) {
-        let x = opts.scale * points[i][0];
-        let y = opts.scale * points[i][1];
+    for (let i = 0; i < points.length; i++) {
+        const x = opts.scale * points[i][0];
+        const y = opts.scale * points[i][1];
 
-        let offset = (opts.canvasSize - opts.scale) / 2;
+        const offset = (opts.size - opts.scale) / 2;
         drawHexagon(ctx, x + offset, y + offset, opts.scale);
     }
 }
 
 function drawHexagon(ctx, x, y, r) {
-    let a = -Math.sqrt(3) / 2 * r;
-    let b = r / 2;
+    const a = -Math.sqrt(3) / 2 * r;
+    const b = r / 2;
 
     ctx.beginPath();
 
